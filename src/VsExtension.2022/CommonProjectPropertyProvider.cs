1419266d7cd7d28dd5a2bde60352bb93d336da1c
DEGROUX, Mathieu <mathieu.degroux-prestataire@ca-cib.com>
2022-12-29 15:47:21 +0100

    Add Support for VS2022

---
+++ src/VsExtension.2022/CommonProjectPropertyProvider.cs
@@ -0,0 +1,120 @@
+﻿extern alias LegacyDTE;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using InliningAnalyzer;
+using LegacyDTE::EnvDTE;
+using Microsoft.VisualStudio.ProjectSystem.Properties;
+using VsExtension.Common;
+
+namespace VsExtension.Vs2022
+{
+    public class CommonProjectPropertyProvider : IProjectPropertyProvider
+    {
+        private readonly Project _project;
+        private readonly IOptionsProvider _optionsProvider;
+        private string _outputPath;
+        private string _assemblyName;
+
+        public CommonProjectPropertyProvider(Project project, IOptionsProvider optionsProvider)
+        {
+            _project = project;
+            _optionsProvider = optionsProvider;
+        }
+
+        public async Task LoadProperties()
+        {
+            var context = (IVsBrowseObjectContext)_project;
+            var unconfiguredProject = context.UnconfiguredProject;
+            var configuredProject = await unconfiguredProject.GetSuggestedConfiguredProjectAsync();
+            var properties = configuredProject.Services.ProjectPropertiesProvider.GetCommonProperties();
+
+            IsOptimized = bool.Parse(await properties.GetEvaluatedPropertyValueAsync("Optimize"));
+            Prefer32Bit = bool.Parse(await properties.GetEvaluatedPropertyValueAsync("Prefer32bit"));
+            PlatformTarget = await properties.GetEvaluatedPropertyValueAsync("PlatformTarget");
+            string isWebSdkRaw = await properties.GetEvaluatedPropertyValueAsync("UsingMicrosoftNETSdkWeb");
+            IsWebSdkProject = string.IsNullOrEmpty(isWebSdkRaw) ? false : bool.Parse(isWebSdkRaw);
+
+            _outputPath = await properties.GetEvaluatedPropertyValueAsync("OutputPath");
+            _assemblyName = await properties.GetEvaluatedPropertyValueAsync("AssemblyName");
+
+            string targetFramework = await properties.GetEvaluatedPropertyValueAsync("TargetFramework");
+            string targetFrameworks = await properties.GetEvaluatedPropertyValueAsync("TargetFrameworks");
+
+            List<string> frameworks = string.IsNullOrEmpty(targetFrameworks) ? new List<string> { targetFramework } : targetFrameworks.Split(';').ToList();
+            TargetFramework = DetermineTargetFramework(frameworks);
+        }
+
+        private string DetermineTargetFramework(List<string> targetFrameworks)
+        {
+            if (targetFrameworks.Count == 1)
+            {
+                // only one targetFramework in project file
+                return targetFrameworks[0];
+            }
+
+            // check if preferred framework from options dialog is in project file
+            var configured = targetFrameworks.FirstOrDefault(t => t.StartsWith(_optionsProvider.PreferredTargetFramework ?? ""));
+            if (configured != null)
+                return configured;
+
+            // derive preferred framework from preferred runtime
+            string runtimePreferredTarget = _optionsProvider.PreferredRuntime == TargetRuntime.NetCore ? "netcore" : "net4";
+            string fallbackTarget = _optionsProvider.PreferredRuntime == TargetRuntime.NetCore ? "net4" : "netcore";
+
+            var runtimePreferred = targetFrameworks.FirstOrDefault(t => t.StartsWith(runtimePreferredTarget));
+            if (runtimePreferred != null)
+                return runtimePreferred;
+
+            var netStandardTarget = targetFrameworks.FirstOrDefault(t => t.StartsWith("netstandard"));
+            if (netStandardTarget != null)
+                return netStandardTarget;
+
+            var secondary = targetFrameworks.FirstOrDefault(t => t.StartsWith(fallbackTarget));
+            if (secondary != null)
+                return secondary;
+
+            throw new JitCompilerException("No compatible TargetFramework could be determined from the project file. At least one of netstandard*, netcore* or net* is required.");
+        }
+
+        public bool IsOptimized { get; private set; }
+        public bool Prefer32Bit { get; private set; }
+        public string PlatformTarget { get; private set; }
+        public string ProjectPath => _project.Properties.Item("FullPath").Value.ToString();
+        public string OutputPath => Path.Combine(ProjectPath, _outputPath);
+        public string TargetFramework { get; set; }
+        public bool IsWebSdkProject { get; set; }
+
+        public string GetOutputFilename(string publishPath)
+        {
+            string assemblyName = _assemblyName + ".dll";
+
+            if (publishPath != null)
+                return Path.Combine(publishPath, assemblyName);
+
+            if (OutputPath.Trim('\\').EndsWith(TargetFramework))
+                return Path.Combine(OutputPath, assemblyName);
+
+            return Path.Combine(OutputPath, "..", TargetFramework, assemblyName);
+        }
+
+        public TargetRuntime TargetRuntime
+        {
+            get
+            {
+                if (TargetFramework.StartsWith("netstandard"))
+                    return _optionsProvider.PreferredRuntime;
+
+                if (TargetFramework.StartsWith("netcore"))
+                    return TargetRuntime.NetCore;
+
+                return TargetRuntime.NetFramework;
+            }
+        }
+        public static bool IsNewProjectFormat(Project vsProject)
+        {
+            return vsProject is IVsBrowseObjectContext;
+        }
+    }
+}
---
+++ src/VsExtension.2022/CommonProjectPropertyProviderFactory2022.cs
@@ -0,0 +1,21 @@
+﻿extern alias LegacyDTE;
+using System.Composition;
+using LegacyDTE::EnvDTE;
+using VsExtension.Common;
+
+namespace VsExtension.Vs2022
+{
+    [Export(typeof(ICommonProjectPropertyProviderFactory))]
+    public class CommonProjectPropertyProviderFactory2022 : ICommonProjectPropertyProviderFactory
+    {
+        public IProjectPropertyProvider Create(Project vsProject, IOptionsProvider optionsProvider)
+        {
+            return new CommonProjectPropertyProvider(vsProject, optionsProvider);
+        }
+
+        public bool IsNewProjectFormat(Project vsProject)
+        {
+            return CommonProjectPropertyProvider.IsNewProjectFormat(vsProject);
+        }
+    }
+}
